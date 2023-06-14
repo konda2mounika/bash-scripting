dba7f04f05de01bb6b7ff34d5a386af743f7aa21
mounika <konda2mounika@gmail.com>
2023-06-14 15:16:41 +0530

    i love uyy

---
+++ 04-vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+a=10
+# a is 10 and is integer
+b=abc
+# abc is a string
+
+
+#### No Datatypes in bash scripting.Everything is a string by defaults
+
+echo value of ais :$a
+echo ${a}
+echi "${b}
