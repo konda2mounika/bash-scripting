a21598d9ef3ef260fa4e97d8bf71adbadfa6d4a8
mounika <konda2mounika@gmail.com>
2023-06-19 02:01:08 +0530

    bash

--- 02-print.sh
+++ 02-print.sh
@@ -4,7 +4,7 @@ echo "line1"
 echo "line2"
 echo -e "line12\nline13"
 echo -e "line12\tline13"
-echo -e "line123\nline234\tline345
+echo -e "line123\nline234\tline345"
 
 ###Escape sequence character:
 #\n:new line
