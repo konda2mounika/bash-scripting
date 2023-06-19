a59c653c115a6f8a52a18b6c6466db30bc7f62f7
mounika <konda2mounika@gmail.com>
2023-06-19 18:12:47 +0530

    roboshop

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 set -e  # Ensure your scriptwill stop if any of the instruction fails
+
 source components/common.sh
 
+echo "installing Nginx: "
 yum install nginx -y    >> /tmp/frontend.log
-systemctl enable nginx
-systemctl start nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+echo -e "\e[42;33m Success \e[0m"
+else
+echo -e "\e[42;33m Failure.Look for the logs \e[0m"
+fi
 
+systemctl enable nginx
+echo "starting Nginx: "
+systemctl start nginx
+
+if [ $? -eq 0 ] ; then
+echo -e "\e[42;33m Success \e[0m"
+else
+echo -e "\e[42;33m Failure.Look for the logs \e[0m"
+fi
+
+echo "Downloading the Nginx: "
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ] ; then
+echo -e "\e[42;33m Success \e[0m"
+else
+echo -e "\e[42;33m Failure.Look for the logs \e[0m"
+fi
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip   >> /tmp/frontend.log
+unzip -o /tmp/frontend.zip   >> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
