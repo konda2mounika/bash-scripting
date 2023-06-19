1a202747d81d92bca07004b2e9bf7eeacb6b3a13
mounika <konda2mounika@gmail.com>
2023-06-19 20:10:12 +0530

    roboshop

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -9,15 +9,15 @@ echo -n "Configuring the Reverse: "
 curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
 stat $?
 
-echo -n "installing ${COMPONENT}: "
-yum install -y mongodb-org  >> /tmp/${COMPONENT}.log
+echo -n "installing $COMPONENT: "
+yum install -y mongodb-org   >> /tmp/${COMPONENT}.log
 stat $?
 
-echo -n "Updating the ${COMPONENT} Config: "
+echo -n "Updating the $COMPONENT Config: "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
-echo -n "start the $COMPONENT service"
+echo -n "start the $COMPONENT service: "
 systemctl enable mongd  >> /tmp/${COMPONENT}.log
 systemctl start mongod
 stat $?
