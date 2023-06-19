6f7e86309048e8d4ffd3d45630b32d4d8a6ac990
mounika <konda2mounika@gmail.com>
2023-06-19 20:16:49 +0530

    roboshop

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -17,7 +17,8 @@ echo -n "Updating the $COMPONENT Config: "
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat $?
 
-echo -n "start the $COMPONENT service "
+echo -n "start the $COMPONENT service: "
+systemctl daemon-reload
 systemctl enable mongdb  >> /tmp/${COMPONENT}.log
 systemctl start mongodb
 stat $?
