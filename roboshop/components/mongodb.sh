71d0c77d9ae29fa5f7d62b230c1a6c9a3dd7421b
mounika <konda2mounika@gmail.com>
2023-06-19 20:43:15 +0530

    roboshop

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -18,10 +18,11 @@ echo -n "Updating the $COMPONENT Config:"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
 stat $? 
 
-echo -n "start the $COMPONENT service: "
+echo -n "Start the $COMPONENT service: "
 systemctl enable mongdb  >> /tmp/${COMPONENT}.log
 systemctl start mongodb
 stat $?
+
 
 echo -n "Downloading the Nginx: "
 curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
