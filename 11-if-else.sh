0111a436603656470a685394c1ae3ace77a678aa
mounika <konda2mounika@gmail.com>
2023-06-19 15:58:11 +0530

    bash

--- 11-if-else.sh
+++ 11-if-else.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 ACTION=$1
-#demo on if else
+# demo on if else
 
-if [ "$ACTION" = "start" ] ; then   
-    echo "Selected choise is start"
-else
-    echo "only valid option is start "
+# if [ "$ACTION" = "start" ] ; then   
+#     echo "Selected choise is start"
+# else
+#     echo "only valid option is start "
+# fi
+
+#Demo on Else if
+if [ "$ACTION" = "start" ] ; then
+    echo " Starting XYZ Service "
+
+    elif [ "$ACTION" = "stop" ] ; then
+        echo " Stopping XYZ Service "
+    elif [ "$ACTION" = "restart" ] ; then
+        echo " Restarting XYZ Service "
+    echo -e "\e[42;33m Valid options are start or stop or restart only \e[0m"
+    
 fi
+     
