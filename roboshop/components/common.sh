f260fa3f84e6307bfef0394b15e1cd908829180a
mounika <konda2mounika@gmail.com>
2023-06-19 19:20:32 +0530

    roboshop

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -8,8 +8,8 @@ fi
 #Declaring the stat function
 stat() {
     if [ $? -eq 0 ] ; then
-        echo -n -e "\e[42;33m Success \e[0m"
+        echo -e -n "\e[42;33m Success \e[0m"
     else
-        echo -n "\e[42;33m Failure.Look for the logs \e[0m"
+        echo -e -n "\e[42;33m Failure.Look for the logs \e[0m"
     fi
 }
