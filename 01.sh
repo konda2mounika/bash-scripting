e4695c859bc62b32cad816fefdb8c70d78532b1d
mounika <konda2mounika@gmail.com>
2023-06-19 12:51:04 +0530

    bash

---
+++ 01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+a=10 
+b=$1      # $1 first argement passed to the script 
+c=$2 
+d=$3 
+echo value of a is : $a 
+echo value of b is : $b 
+echo value of c is : $c 
+echo value of d is : $d 
+# sh scriptname.sh 100 200 300 
+# sh scriptname.sh  $1  $2  $3
