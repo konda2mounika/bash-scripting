a74f7d7a95b1c7993eb620b6da7c20d375a8ed5a
mounika <konda2mounika@gmail.com>
2023-06-19 12:58:49 +0530

    bash

--- 01.sh
+++ 01.sh
@@ -5,9 +5,9 @@ a=10
 b=$1      # $1 first argement passed to the script 
 c=$2 
 d=$3 
-echo value of a is : $a 
-echo value of b is : $b 
-echo value of c is : $c 
-echo value of d is : $d 
+echo -e "\e[44;33m value of a is : $a \e[0m " 
+echo -e "\e[44;33m value of a is : $b \e[0m " 
+echo -e "\e[44;33m value of a is : $c \e[0m " 
+echo -e "\e[44;33m value of a is : $d \e[0m " 
 # sh scriptname.sh 100 200 300 
 # sh scriptname.sh  $1  $2  $3
