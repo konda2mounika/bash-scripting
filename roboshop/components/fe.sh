76bc7661278e2f7a0d7e4df53a4803765d2f423e
mounika <konda2mounika@gmail.com>
2023-07-12 18:06:36 +0530

    fe

---
+++ roboshop/components/fe.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e   # ensure your script will stop if any of the instruction fails
+
+source components/common.sh
+
+echo -n "Installing Nginx: "
+yum install nginx -y   >> /tmp/frontend.log 
+
+systemctl enable nginx 
+
+echo -n "Starting Nginx: "
+systemctl start nginx 
+stat $?
+
+echo -n "Downloading the Code"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat $?
+
+cd /usr/share/nginx/html
+rm -rf *
+echo -n "Extracting the zip file:"
+unzip -o /tmp/frontend.zip >> /tmp/frontend.log
+stat $? 
+
+mv frontend-main/* .
+mv static/* .
+echo -n "Performing Cleanup: "
+rm -rf frontend-main README.md
+stat  $?
+
+
+echo -n "Starting Ngnix: "
+systemctl restart nginx
+stat $?
+
+# source is a command to import a file and run it locally
